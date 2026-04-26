0ea5c037c4f3d902b3ce9301dbbd59d6909713af
DanielPino194 <daniel.pino@fedea.com.ar>
2026-04-25 21:47:31 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,12 @@
-const CACHE='liqtotal-v1';
+const CACHE='liqtotal-v3';
 self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+    )).then(()=>self.clients.claim())
+  );
+});
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
   e.respondWith(
